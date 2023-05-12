469fd83b9cffbbbc1af931daf31dcad2670955ca
Andrei Borisov <andrei.borr@gmail.com>
2023-05-12 12:11:23 +0300

    Repository

--- src/main/java/HinernateRunner.java
+++ src/main/java/HinernateRunner.java
@@ -9,23 +9,13 @@ import javax.transaction.Transactional;
 public class HinernateRunner {
     public static void main(String[] args) {
         User user = null;
-        SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
-        try (Session session = sessionFactory.openSession()) {
+
+        try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
+             Session session = sessionFactory.getCurrentSession()) {
             session.beginTransaction();
 
             user = session.find(User.class, 1L);
-            var user1 = session.find(User.class, 1L);
-            System.out.println(user.getCompany());
-            System.out.println(user.getUserChats());
-            session.getTransaction().commit();
-        }
-        try (Session session = sessionFactory.openSession()) {
-            session.beginTransaction();
-
-            var user2 = session.find(User.class, 1L);
-            System.out.println(user2.getCompany());
-           // System.out.println(user.getUserChats().size());
-            System.out.println(user.getUserChats());
+            System.out.println(user);
             session.getTransaction().commit();
         }
     }
